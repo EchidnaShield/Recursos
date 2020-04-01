f8aa939ad2b40ac72dad132bede40733ac7472e7
Jose Pujol Perez <Josepujol@users.noreply.github.com>
2020-04-01 13:28:51 +0200

    Update Fading.ino

--- Didactica/Actividades_IDE_Arduino/BrilloLED/Fading/Fading.ino
+++ Didactica/Actividades_IDE_Arduino/BrilloLED/Fading/Fading.ino
@@ -1,52 +1,39 @@
 /*
-  Fading
+    Programa para encender y apagar LED RGB blanco gradualmente
 
-  This example shows how to fade an LED using the analogWrite() function.
+    LED R pin 9
+    LED G pin 5
+    LED B pin 6
 
-  The circuit:
-  - LED attached from digital pin 9 to ground.
-
-  created 1 Nov 2008
-  by David A. Mellis
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Fading
+    Basado en el programa Fade de Arduino
+    Jose Pujol
+    Mazrzo de 2020
 */
 
+// Variables globales constantes
 // pines del LED RGB
 const int ledRPin = 9; // establece el pin del LED rojo
 const int ledGPin = 5; // establece el pin del LED verde
 const int ledBPin = 6; // establece el pin del LED azul
 
 void setup() {
-  // nothing happens in setup
+  // nada que declarar en el set up
 }
-
 void loop() {
-  // fade in from min to max in increments of 5 points:
+  // Encendido gradual del LED en 2,5s
   for (int fadeValue = 0 ; fadeValue <= 255; fadeValue ++) {
-    // sets the value (range from 0 to 255):
     analogWrite(ledRPin, fadeValue);
     analogWrite(ledGPin, fadeValue);
     analogWrite(ledBPin, fadeValue);
-    // wait for 30 milliseconds to see the dimming effect
-    delay(30);
+    delay(50);                        // espera de 50 ms
   }
-
-  
-
-  // fade out from max to min in increments of 5 points:
+  // Apagado gradual del LED en 2,5
   for (int fadeValue = 255 ; fadeValue >= 0; fadeValue --) {
-    // sets the value (range from 0 to 255):
     analogWrite(ledRPin, fadeValue);
     analogWrite(ledGPin, fadeValue);
     analogWrite(ledBPin, fadeValue);
-    // wait for 30 milliseconds to see the dimming effect
-    delay(30);
+    delay(50);                        // espera de 50 ms
   }
-
-  delay(500);
+  // tiempo de espera apagado
+  delay(1000);
 }

b10c5b15d02d6c848ca7400b2d75fda0e9d51e00
Jose Pujol Perez <Josepujol@users.noreply.github.com>
2020-04-07 18:31:23 +0200

    Update Monitorizacion_acelerometro.ino

--- Didactica/Actividades_IDE_Arduino/Acelerometro/Monitorizacion_acelerometro/Monitorizacion_acelerometro.ino
+++ Didactica/Actividades_IDE_Arduino/Acelerometro/Monitorizacion_acelerometro/Monitorizacion_acelerometro.ino
@@ -1,23 +1,30 @@
 /*
   Programa para monitorizar el valor del acelerómetro
+  por el serial plotter
+
+  A2 eje X acelerómetro
+  A3 eje Y acelerómetro
+  
   Noviembre 2017
   JosePujol
 */
 
-const int xPin = A2;    // pin donde conectamos el sensor analogico
-const int yPin = A3;    // pin donde conectamos el sensor analogico
-int xValue = 0;  // variable para alamacenar los valores del sensor
-int yValue = 0;  // variable para alamacenar los valores del sensor
+// variables constantes
+const int xPin = A2;    // pin al que va conectado eje X acelerómetro
+const int yPin = A3;    // pin al que va conectado eje Y acelerómetro
+// variables que cambian
+int xValue = 0;  // variable para almacenar los valores X acelerómetro
+int yValue = 0;  // variable para almacenar los valores Y acelerómetro
 
 void setup() {
   Serial.begin(9600);        // abrimos el puerto serie y configuramos velocidad
 }
 
 void loop() {
-  xValue = analogRead(xPin);    // lee el valor del sensor
-  yValue = analogRead(yPin);    // lee el valor del sensor
-  Serial.print(xValue);         // Imprime el valor del sensor
+  xValue = analogRead(xPin);    // lee y almacena el valor del eje X acelerometro 
+  yValue = analogRead(yPin);    // lee y almacena el valor del eje Y acelerometro 
+  Serial.print(xValue);         // Imprime el valor del eje X acelerometro
   Serial.print("\t");           // Imprime un tabulador
-  Serial.println(yValue);       // Imprime el valor del sensor
+  Serial.println(yValue);       // Imprime el valor del eje Y acelerometro
   delay(100);                    // Espera 0.1 segundo
 }

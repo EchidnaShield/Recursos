8f9231a4bd38755459743d35c97dec6f7bd8278c
XDeSIG <xdesig@gmail.com>
2020-04-12 20:03:41 +0200

    Update Vumetro_W

--- Didactica/Actividades_IDE_Arduino/Microfono/Vumetro_W/Vumetro_W.ino
+++ Didactica/Actividades_IDE_Arduino/Microfono/Vumetro_W/Vumetro_W.ino
@@ -8,7 +8,7 @@
 #include "config_W.h" // Definición de todos los recursos de Echidna Shield "S" o White "W"
 
 
-int ledPins[] = {L_Gre, L_Orn, L_Red, RGB_R, RGB_G, RGB_B} ; // LEDes en una matriz
+int ledPins[] = {255, L_Gre, L_Orn, L_Red, RGB_R, RGB_G, RGB_B} ; // LEDes en una matriz, el 
 
 #define Pin_analox A8
 
@@ -17,7 +17,7 @@ int V_Max = 20; // valor maximo que queremos medir
 void setup()
 {
   //Definimos los LEDes como salidas
-  for (int i = 0; i < 6; i++)
+  for (int i = 0; i < 7; i++)
   {
     pinMode(ledPins[i], OUTPUT);
   }
@@ -36,7 +36,7 @@ void loop()
   }
 
   // convierte los valores del MIC a un nº de 0 a 6 (Leds)
-  int LED_maior = map(sinal, 0, V_Max, 0, 6); //mapea(valor, Valor inferior de entrada, superior de entrada, a bajo, a alto)
+  int LED_maior = map(sinal, 0, V_Max, 0, 7); //mapea(valor, Valor inferior de entrada, superior de entrada, a bajo, a alto)
 
   for (int i = 0; i < LED_maior; i++) //Enciende los LEDes hasta e valor más alto
   {
@@ -46,7 +46,7 @@ void loop()
     analogWrite (ledPins[0], 16);
   }
 
-  for (int i = LED_maior + 1; i < 6; i++) //Apaga hasta el LED más alto
+  for (int i = LED_maior + 1; i < 7; i++) //Apaga hasta el LED más alto
   {
     digitalWrite(ledPins[i], LOW);
   }

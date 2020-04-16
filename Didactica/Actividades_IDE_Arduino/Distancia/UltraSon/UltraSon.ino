be892c1531cfffaee0af54e04d8e95fe69dd1a57
XDeSIG <xdesig@gmail.com>
2020-04-16 18:54:36 +0200

    Update UltraSon.ino

--- Didactica/Actividades_IDE_Arduino/Distancia/UltraSon/UltraSon.ino
+++ Didactica/Actividades_IDE_Arduino/Distancia/UltraSon/UltraSon.ino
@@ -35,17 +35,17 @@ void setup() {
 }
 
 void loop() {
-  pinMode (IN, OUTPUT); //Define el pin como salida "Trigger" 
-  digitalWrite(IN, LOW); //Trigger en estado bajo y espera 2 ms
+  pinMode (IO1, OUTPUT); //Define el pin como salida "Trigger" 
+  digitalWrite(IO1, LOW); //Trigger en estado bajo y espera 2 ms
   delayMicroseconds(2); 
-  digitalWrite(IN, HIGH);//Trigger a estado alto y espera 10 ms
+  digitalWrite(IO1, HIGH);//Trigger a estado alto y espera 10 ms
   delayMicroseconds(10);
-  digitalWrite(IN, LOW);   //Trigger en estado bajo.
+  digitalWrite(IO1, LOW);   //Trigger en estado bajo.
 
-  pinMode (IN, INPUT); //Define el pin como entrada esperando el ECHO
+  pinMode (IO1, INPUT); //Define el pin como entrada esperando el ECHO
   
   //  La función pulseIn obtiene el tiempo que tarda en cambiar entre estados, en este caso LOW a HIGH
-  unsigned long tiempo = pulseIn(IN, HIGH);
+  unsigned long tiempo = pulseIn(IO1, HIGH,170000);
   
    // Obtenemos la distancia en cm,el tiempo lo tenemos en microsegundos lo pasaremos a segundos (1E-6 = 0.000001)
   

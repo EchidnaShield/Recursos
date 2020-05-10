faf40e42e8ab91a56801b1ecffeddc4f71850612
Jose Pujol Perez <Josepujol@users.noreply.github.com>
2020-05-10 10:46:39 +0200

    Update lecturaMkMk.ino

--- Didactica/Actividades_IDE_Arduino/MkMk/lecturaMkMk/lecturaMkMk.ino
+++ Didactica/Actividades_IDE_Arduino/MkMk/lecturaMkMk/lecturaMkMk.ino
@@ -1,19 +1,26 @@
 /* Lectura Makey Makey
-  Programa para leer las entradads MkMk
+  Programa para leer las entradas MkMk
 */
+int entradas[] = {A0, A1, A2, A3, A4, A5, 2, 3} ; // Entradas
 
 int n=5;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
+for (int i = 0; i < 8; i++)
+  {
+   // define como entradas los pines MkMk
+    pinMode(entradas[i], INPUT);
+  }
+// si, que las analog inputs se defin cuando la leemos, pero es una buena costumbre definir antes todo.
+
+ // pinMode(2, INPUT);
+ // pinMode(3, INPUT);
   Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
   int a0Value = analogRead(A0);
   int a1Value = analogRead(A1);
   int a2Value = analogRead(A2);
@@ -59,3 +66,5 @@ void loop() {
   delay(1000);
 
 }
+
+

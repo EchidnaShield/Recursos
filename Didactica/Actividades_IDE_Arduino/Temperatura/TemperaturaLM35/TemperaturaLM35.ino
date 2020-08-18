4694d9028516395cbfe976c76132fd57911400f9
Jose Pujol Perez <Josepujol@users.noreply.github.com>
2020-08-18 12:43:12 +0200

    Update TemperaturaLM35.ino

--- Didactica/Actividades_IDE_Arduino/Temperatura/TemperaturaLM35/TemperaturaLM35.ino
+++ Didactica/Actividades_IDE_Arduino/Temperatura/TemperaturaLM35/TemperaturaLM35.ino
@@ -1,20 +1,18 @@
 /*
  Medida de temperatura con sensor LM35
  Mostramos la temperatura por el monitor serie
- Usamos analogReference para aumentar la precision del sensor
+ Usamos analogReference para aumentar la precision de la medida del sensor
+ Referencia 1.1 valido para micro Atmega328= Arduino UNO
 
- Descripción
-
- Sensor LM35 
+ Sensor LM35: 
  pin V del sensor conectado a 5V de Arduino
- pin central del sensor Vout a A0 de Arduino
+ pin central del sensor Vout a A6 de Arduino
  pin GND a GND de Arduino
  
  */
 
 // constantes que no cambian
-// se usan para establecer los pines
-const int LM35Pin = A6;     // Pin al que conectamos sensor temperatura
+const int LM35Pin = A6;     // pin al que conectamos sensor temperatura
 
 // variables globales que cambian
 float temperatura = 0;         // variable para almacenar la temperatura medida
@@ -22,18 +20,18 @@ float temperatura = 0;         // variable para almacenar la temperatura medida
 // Configuración
 void setup() {
   
-  analogReference(INTERNAL); // cambiamos la referencia analogica a 1.1v
-  Serial.begin(9600); // Configuramos la conexion con el PC
+  analogReference(INTERNAL); // cambiamos la referencia analogica a 1.1V (AtMega328)
+  Serial.begin(9600); // configuramos la velocidad de conexion con el puerto serie del PC
 
 }
 
 // Bucle que se repite
 void loop() {
-  int lectura = analogRead(LM35Pin); // Valor entre 0 y 1023
-  temperatura = (lectura* 1.1 * 100.0)/1024.0; // Temperatura en º Celsius
+  int lectura = analogRead(LM35Pin); // valor entre 0 y 1023
+  temperatura = (lectura* 1.1 * 100.0)/1024.0; // temperatura en º Celsius
 
-  Serial.print("Temperatura:");  
-  Serial.print(temperatura);  // Mostramos el valor de la temperatura 
-  Serial.println("C");
-  delay(1000);
+  Serial.print("Temperatura: ");  
+  Serial.print(temperatura);  // mostramos el valor de la temperatura 
+  Serial.println(" ºC");
+  delay(1000);              // tiempo de espera de 1s
 }

2410173405ff95689ef9df53974650e51e8299ab
XDeSIG <xdesig@gmail.com>
2021-03-10 13:32:18 +0100

    Update Circulo_OLED.ino

--- Didactica/Actividades_IDE_Arduino/OLED/Circulo_OLED/Circulo_OLED.ino
+++ Didactica/Actividades_IDE_Arduino/OLED/Circulo_OLED/Circulo_OLED.ino
@@ -4,7 +4,11 @@
 #include "config_B.h" // Definición de todolos recursos de Echidna Shield "S",  White "W" ou Black "B"
 
 #include <OLED_I2C.h> //Copyright (C)2015-2019 Rinky-Dink Electronics, Henning Karlsen.CC BY-NC-SA 3.0 license.
-OLED  myOLED(7, A4);  //establece os pines de comunicacións I2C OLED
+OLED  myOLED(7, A4);  //establece os pines de comunicacións I2C OLED (SCL, SDA)
+extern uint8_t SmallFont[];
+extern uint8_t MediumNumbers[];
+
+
 
 
 //******** Establece as entradas de sinal a medir *****
@@ -13,7 +17,7 @@ OLED  myOLED(7, A4);  //establece os pines de comunicacións I2C OLED
 #define EntradaY Joy_Y
 
 //******** Establece los valores minimos y maximos 
-//para adapartlos a los valores de las entradas.
+//para adapatalos a los valores de las entradas.
 const int VXmin = 0;
 const int VXmax = 1023;
 const int VYmin = 0;
@@ -22,8 +26,8 @@ const int VYmax = 1023;
 //*****************************************************
 
 
-const int Radio =   15;   //Radio da semicircunferencia do cuadrante (valor clave). Por defecto 90.
-int PosX = 64; //coordenada centro X do cuadrante. Por defecto 62.
+const int Radio =   5;   //Radio da semicircunferencia do cuadrante (valor clave). Por defecto 15.
+int PosX = 64; //coordenada centro X do cuadrante. Por defecto 64.
 int PosY = 32; //coordenada centro Y do cuadrante. Por defecto 32.
 
 
@@ -47,11 +51,16 @@ void setup()
   Serial.begin(9600);         // Inicializa a comunicación serie
 
   pinMode(EntradaX, INPUT);
+  pinMode(EntradaY, INPUT);
 
   //analogReference(INTERNAL);    // Cambia a referencia do conversor analoxico - dixital
 
   //https://www.arduino.cc/reference/en/language/functions/analog-io/analogreference/
 
+  myOLED.setFont(SmallFont);                //*** elixe a fonte de letra  
+  myOLED.print("Iniciando", CENTER, 32);     // Escribe na memoria da OLED Centrada na liña 5
+  myOLED.update();
+  delay(2000);
 }
 
 
@@ -67,8 +76,8 @@ void loop () {
   Serial.println (MedidaY);
 
   //************** Mapea los valores a los limites de la pantalla ******
-  PosX = map( MedidaX, VXmin, VXmax, 0 , 128 );
-  PosY = map( MedidaY, VYmin, VYmax, 64 , 0 );
+  PosX = map( MedidaX, VXmin, VXmax, 0 , 127 );
+  PosY = map( MedidaY, VYmin, VYmax, 63 , 0 );
 
   //************** Dibuja un rectangulo ***********************
   myOLED.drawRect(0, 0, 127, 63); // coordenadas inicio x,y fin x,y

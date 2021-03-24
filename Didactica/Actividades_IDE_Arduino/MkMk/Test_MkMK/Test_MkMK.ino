f481ef8a804ff4f0ba1a6d92521c10cb363fa8a5
XDeSIG <xdesig@gmail.com>
2021-03-24 13:42:26 +0100

    Update Test_MkMK.ino

--- Didactica/Actividades_IDE_Arduino/MkMk/Test_MkMK/Test_MkMK.ino
+++ Didactica/Actividades_IDE_Arduino/MkMk/Test_MkMK/Test_MkMK.ino
@@ -1,54 +1,51 @@
 /* Lectura Makey Makey
   Programa para leer las entradas MkMk
 */
+
 int entrada[] = {A0, A1, A2, A3, A4, A5, 2, 3} ; //Entradas
+
+//char *texto[] = {"A0", "A1", "A2", "A3", "A4", "A5", "D2", "D3"} ; // texto a representar para cada entrada
+char* texto[] = {"Mk1", "Mk2", "Mk3", "Mk4", "Mk5", "Mk6", "Mk7", "Mk8"} ; // texto a representar para cada entrada
 int V_MkMk[8];
 int n = 5;
 int i = 0;
 
 void setup() {
-  for (i = 0; i < 8; i++)
+  for (int i = 0; i < 8; i++)
   {
     //Las entradas se definen por defecto, pero es una buena costumbre definir antes todo.
-    
+
     pinMode(entrada[i], INPUT); // define como entradas los pines MkMk
   }
-  Serial.begin(9600);
+
+  Serial.begin(9600); // iniciamos la comunicación serie a 9600bits por segundo
 
 }
 
 void loop() {
-  for (i = 0; i < 6; i++) {
-    V_MkMk[i] = analogRead(entrada[i]);
-    }
-    V_MkMk[6] = digitalRead(entrada[6]);
-    V_MkMk[7] = digitalRead(entrada[7]);
-    
-  if (n == 5) {
-    n = 0;
-    Serial.println();   //Linea de separacion
-    Serial.print("A0");
-    Serial.print("\t");
-    Serial.print("A1");
-    Serial.print("\t");
-    Serial.print("A2");
-    Serial.print("\t");
-    Serial.print("A3");
-    Serial.print("\t");
-    Serial.print("A4");
-    Serial.print("\t");
-    Serial.print("A5");;
-    Serial.print("\t");;
-    Serial.print("D2");
-    Serial.print("\t");
-    Serial.println("D3");
+  
+// Lectura de las entrada0 a entrada7, depositando su valor en V_MkMk0 a V_MkMk8
+  for (i = 0; i < 8 ; i++) {
+    V_MkMk[i] = digitalRead(entrada[i]);
   }
-  for (int i = 0; i < 8; i++) {
+
+// Enviamos (serie) el "texto" de las entradas, solo cada cinco lecturas.
+  if (n == 5) {
+    n = 0;                        //Borramos el contador de lecturas
+    Serial.println();             //Una linea de separación cada cinco lecturas
+    
+    for (i = 0; i < 8; i++) {
+      Serial.print(texto[i]);
+      Serial.print("\t");
+    }
+     Serial.println();
+  }
+  // Enviamos serie el valor de las entradas
+  for (i = 0; i < 8; i++) {
     Serial.print (V_MkMk[i]);
     Serial.print("\t");
   }
-  Serial.println("");
-  n++;
+  Serial.println();   
+  n++;                  //Incrementamos el contador de lecturas
   delay(1000);
-
 }
